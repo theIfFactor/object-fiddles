9e221c37bbfdc4ee2fbdc9e339c28fb571e937ed
Daniel Valentine <mrdanvalentine@gmail.com>
2017-08-08 13:54:16 -0600

    first problem

--- practice.js
+++ practice.js
@@ -1,10 +1,17 @@
 //Once you complete a problem, open up Chrome and check the answer in the console.
 
 
-//Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
+//Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. 
+//Then alert your name using dot notation.
 
   //Code here
 
+var me = {
+	name: "Daniel Valentine",
+	age: 23
+};
+
+alert(me.name);
 
 
 
